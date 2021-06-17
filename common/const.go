84edc939287d0d9cfd07fea5974624afb012edde
LinskRuis.32G <mgr9525@qq.com>
2021-06-17 14:48:37 +0800

    commands

--- common/const.go
+++ common/const.go
@@ -1,5 +1,7 @@
 package common
 
+import "regexp"
+
 const (
 	TimeFmt  = "2006-01-02 15:04:05"
 	TimeFmts = "2006-01-02"
@@ -9,4 +11,9 @@ const (
 	MaxTopicLen   = 1024 * 1024 * 10
 	MaxCliHeadLen = 1024 * 1024 * 100
 	MaxCliBodyLen = 1024 * 1024 * 1024
+)
+
+var (
+	RegName      = regexp.MustCompile("[`~!@#$%^&*()\\+=<>?:\"{}|,\\.\\/;'\\\\[\\]·~！@#￥%……&*（）——={}|《》？：“”【】、；‘'，。、]")
+	RegNameSpace = regexp.MustCompile("[`~!@#$%^&*()\\+=<>?:\"{}|,\\.;'\\\\[\\]·~！@#￥%……&*（）——={}|《》？：“”【】、；‘'，。、]")
 )
