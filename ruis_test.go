73c5cf09420db64d0d2d397e3ce67e1d143775e0
LinskRuis.32G <mgr9525@qq.com>
2021-06-18 12:30:48 +0800

    timer

--- ruis_test.go
+++ ruis_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/yggworldtree/go-core/bean"
 	"github.com/yggworldtree/go-core/common"
 	"testing"
+	"time"
 )
 
 func Test1(t *testing.T) {
@@ -30,4 +31,25 @@ func Test3(t *testing.T) {
 	s := "123ms"
 	ms := common.RegTms.FindAllStringSubmatch(s, -1)[0]
 	println(ms)
+}
+func Test4(t *testing.T) {
+	n := 0
+	tmr := time.NewTicker(time.Second)
+	go func() {
+		for {
+			time.Sleep(time.Millisecond * 100)
+			if n > 5 {
+				tmr.Stop()
+				break
+			}
+		}
+	}()
+	for {
+		<-tmr.C
+		n++
+		println("tick:", n)
+		if n > 10 {
+			break
+		}
+	}
 }
---
+++ utils/timer.go
@@ -0,0 +1,26 @@
+package utils
+
+import "time"
+
+type Timer struct {
+	tms time.Time
+	tmd time.Duration
+}
+
+func NewTimer(tmd time.Duration, now ...bool) *Timer {
+	c := &Timer{
+		tmd: tmd,
+	}
+	if len(now) > 0 && now[0] {
+		c.tms = time.Now()
+	}
+	return c
+}
+func (c *Timer) Tick() bool {
+	if time.Since(c.tms) < c.tmd {
+		return false
+	} else {
+		c.tms = time.Now()
+		return true
+	}
+}
