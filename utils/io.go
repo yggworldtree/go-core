5dd5d4bef75ad0694ebbef9e971012a44627a8d4
LinskRuis.32G <mgr9525@qq.com>
2021-06-18 10:41:40 +0800

    -util

--- utils/io.go
+++ utils/io.go
@@ -1,19 +1,9 @@
 package utils
 
 import (
-	"context"
 	"crypto/md5"
 	"encoding/hex"
 )
-
-func EndContext(ctx context.Context) bool {
-	select {
-	case <-ctx.Done():
-		return true
-	default:
-		return false
-	}
-}
 
 func Md5String(data string) string {
 	m5 := md5.New()
